52157d390182e978b9ee7ac2e9ef1a75d057b267
xidinor <serviceinfo@versatile.space>
2025-08-12 22:06:44 +0900

    fix variable
     - private_subnet_cidrs => public_subnet_cidrs

--- variables.tf
+++ variables.tf
@@ -1,35 +1,34 @@
-// variables.tf
 variable "region" {
-  description = "AWS EC2 instance region (Japan East)"
+  description = "デプロイ先リージョン"
   type        = string
   default     = "ap-northeast-1"
 }
 
 variable "azs" {
-  description = "Availability zones"
+  description = "利用する AZ（2つ）"
   type        = list(string)
   default     = ["ap-northeast-1a", "ap-northeast-1c"]
 }
 
 variable "vpc_cidr" {
-  description = "VPC Network CIDR Block"
+  description = "VPC の IPv4 CIDR"
   type        = string
-  default     = "172.16.0.0/20"
+  default     = "172.16.0.0/16"
 }
 
-variable "private_subnet_cidrs" {
-  description = "Private subnet CIDR (order by AZ)"
+variable "public_subnet_cidrs" {
+  description = "パブリックサブネットの IPv4 CIDR（AZ順）"
   type        = list(string)
-  default     = ["172.16.0.0/24", "172.16.1.0/24"]
+  default     = ["172.16.1.0/24", "172.16.2.0/24"]
 }
 
 variable "instance_ami" {
-  description = "Windows Server 2025 AMI (standard Windows Server AMI)"
+  description = "EC2 起動に使用する AMI（例：Windows Server 等）"
   type        = string
 }
 
 variable "instance_type" {
-  description = "EC2 instance type"
+  description = "インスタンスタイプ"
   type        = string
   default     = "g6.2xlarge"
 }
